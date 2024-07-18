796a37bead344135d30ba7fd79a392998121ea04
Dan Garfield <dangarfielduk@hotmail.com>
2024-07-18 09:47:08 +0100

    Fix bin events script

--- functions/bins.js
+++ functions/bins.js
@@ -5,22 +5,32 @@ const moment = require('moment')
 require('dotenv').config()
 
 const getDates = async () => {
-  const res = await p(process.env.BINS_URL)
+  const currentDate = new Date();
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const url = process.env.BINS_URL.replace('2024/07/18',`${currentDate.getFullYear()}/${month}/${day}`)
+  console.log('url', url)
+  const res = await p(url)
+  const bodyString = res.body.toString('utf8')
+
   const lines = res.body.toString('utf8').split(/\r?\n/)
-  let currentType = 'unknown'
+    .filter(l => l.includes('<p class="colordark-blue fontfamilyArial fontsize12rem">') && !l.includes('day</p>'))
+    .map(l => l.split('>')[1].replace('</p',''))
+  // console.log('lines', lines)
+
   let dates = []
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i]
-    if (line.includes('Food Waste')) {
+  for (let i = 0; i < lines.length; i=i+2) {
+    console.log('event', i,lines[i],lines[i+1])
+    let currentType = 'unknown'
+    if (lines[i+1].includes('Food Waste')) {
       currentType = 'food'
-    } else if (line.includes('Refuse Collection')) {
+    } else if (lines[i+1].includes('Refuse Collection')) {
       currentType = 'refuse'
-    } else if (line.includes('Recycling Collection')) {
+    } else if (lines[i+1].includes('Recycling Collection')) {
       currentType = 'recycling'
-    } else if (line.includes('arry.push')) {
-      if (currentType === 'refuse' || currentType === 'recycling') {
-        dates.push({type: currentType, dateString: line.split('"')[1]})
-      }
+    }
+    if (currentType === 'refuse' || currentType === 'recycling') {
+      dates.push({type: currentType, dateString: lines[i]})
     }
   }
   return dates
@@ -29,7 +39,7 @@ const getAlertDates = async () => {
   let dates = await getDates()
   console.log('dates', dates)
   let alertDates = dates.map((d) => {
-    let date = moment(d.dateString).subtract(1, 'days')
+    let date = moment(d.dateString,'DD-MM-YYYY').subtract(1, 'days')
     d.week = date.isoWeek()
     d.alarmDateTimeStringStart = `${date.format('YYYY-MM-DD')}T20:30:00.000`
     d.alarmDateTimeStringEnd = `${date.format('YYYY-MM-DD')}T21:30:00.000`
@@ -72,18 +82,23 @@ const addBinEvents = async (auth, alertDates) => {
 const removeExistingBinEvents = async (auth) => {
   try {
     const calendar = google.calendar({version: 'v3', auth})
+    const oneYearFromNow = new Date();
+    oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
     let res = await calendar.events.list({
       calendarId: 'dangarfielduk@gmail.com',
-      timeMin: (new Date()).toISOString(),
+      timeMin: new Date().toISOString(),
+      timeMax: oneYearFromNow.toISOString(),
       maxResults: 100,
       singleEvents: true,
       orderBy: 'startTime'
     })
+    console.log('binEvents', res.data.items.map(i => i.summary), res.data.items.length)
     let binEvents = []
     for (let i = 0; i < res.data.items.length; i++) {
       const event = res.data.items[i]
       if (event.summary.includes('Bin')) {
         // Remove
+        console.log('binEvent', event)
         await calendar.events.delete({
           calendarId: 'dangarfielduk@gmail.com',
           eventId: event.id
